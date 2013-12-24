7dd806fff64f4a6ae9ed0f111dcaf9e9dc2a464d
Yin Hanchun <hanchun.yin@leediancn.com>
2013-12-24 09:40:30 +0800

    improve controller.js

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,14 +1,8 @@
-var fs = require('fs');
-var moment = require('moment');
-var passwordHash = require('password-hash');
 var async = require('async');
 var entityFactory = require('../models/entity-factory');
-var mapper = require('../helpers/mapper');
-var utils = require('../helpers/utils');
 
 var User = entityFactory.User;
 var CodeSnippet = entityFactory.CodeSnippet;
-var SnippetType = entityFactory.SnippetType;
 var UserRelation = entityFactory.UserRelation;
 
 String.prototype.trim = function() {
@@ -38,7 +32,7 @@ module.exports = {
         }
     },
     index: function(req, res, next) {
-        var user = req.user || '';
+        // var user = req.user || '';
         res.render('index', {
             // credential: user,
             token: req.csrfToken()
@@ -90,66 +84,66 @@ function errHandler(err, message, next) {
 function getAmountObj(userId, callback) {
     async.series({
             jsAmount: function(callback) {
-                var whereOpiton = {
+                var opiton = {
                     where: {
                         user_id: userId,
                         type_id: 1
                     }
                 };
-                CodeSnippet.count(whereOpiton).success(function(jsAmount) {
+                CodeSnippet.count(opiton).success(function(jsAmount) {
                     callback(null, jsAmount);
                 });
             },
             javaAmount: function(callback) {
-                var whereOpiton = {
+                var opiton = {
                     where: {
                         user_id: userId,
                         type_id: 2
                     }
                 };
-                CodeSnippet.count(whereOpiton).success(function(javaAmount) {
+                CodeSnippet.count(opiton).success(function(javaAmount) {
                     callback(null, javaAmount);
                 });
             },
             cAmount: function(callback) {
-                var whereOpiton = {
+                var opiton = {
                     where: {
                         user_id: userId,
                         type_id: 3
                     }
                 };
-                CodeSnippet.count(whereOpiton).success(function(cAmount) {
+                CodeSnippet.count(opiton).success(function(cAmount) {
                     callback(null, cAmount);
                 });
             },
             csharpAmount: function(callback) {
-                var whereOpiton = {
+                var opiton = {
                     where: {
                         user_id: userId,
                         type_id: 4
                     }
                 };
-                CodeSnippet.count(whereOpiton).success(function(csharpAmount) {
+                CodeSnippet.count(opiton).success(function(csharpAmount) {
                     callback(null, csharpAmount);
                 });
             },
             followAmount: function(callback) {
-                var whereOpiton = {
+                var opiton = {
                     where: {
                         user_id: userId
                     }
                 };
-                UserRelation.count(whereOpiton).success(function(followAmount) {
+                UserRelation.count(opiton).success(function(followAmount) {
                     callback(null, followAmount);
                 });
             },
             followerAmount: function(callback) {
-                var whereOpiton = {
+                var opiton = {
                     where: {
                         follow_id: userId
                     }
                 };
-                UserRelation.count(whereOpiton).success(function(followerAmount) {
+                UserRelation.count(opiton).success(function(followerAmount) {
                     callback(null, followerAmount);
                 });
             }
